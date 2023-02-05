c9d44b892230d2142ab6b5ab23dbb6c653c155a4
viyeumaden <66916286+viyeumaden@users.noreply.github.com>
2023-02-06 01:38:32 +0700

    Update restart-interface.sh

--- restart-interface.sh
+++ restart-interface.sh
@@ -2,7 +2,7 @@
 # This file is responsible for restarting the network interface.
 # Should be run once OFFLINE state is detected.
 
-INTERFACE="wwan"
+INTERFACE="wwan0"
 
 # syslog entry
 logger -s "INTERNET KEEP ALIVE SYSTEM: Restarting the LTE interface."
